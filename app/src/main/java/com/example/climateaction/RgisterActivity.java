86a732929788b16598a542047ede9663c26c1bd6
Shivansh Kumar <codes.stalker@gmail.com>
2023-07-09 18:29:14 +0530

    Regiter Activty

---
+++ app/src/main/java/com/example/climateaction/RgisterActivity.java
@@ -0,0 +1,14 @@
+package com.example.climateaction;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RgisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rgister);
+    }
+}
