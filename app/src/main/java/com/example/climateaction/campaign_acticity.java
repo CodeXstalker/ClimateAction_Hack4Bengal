4c1f6d42d0df8abbdcfd0c8ea2e9ff71419d1577
sankalp125 <sankalp.tiwari125@gmail.com>
2023-07-09 20:11:59 +0530

    patch 1.5

---
+++ app/src/main/java/com/example/climateaction/campaign_acticity.java
@@ -0,0 +1,14 @@
+package com.example.climateaction;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class campaign_acticity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_campaign_acticity);
+    }
+}
