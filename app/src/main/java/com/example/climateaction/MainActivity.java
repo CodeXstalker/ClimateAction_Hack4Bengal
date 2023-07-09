a551270f4f56102a66962a440799e966ffaa275b
Shivansh Kumar <codes.stalker@gmail.com>
2023-07-10 00:57:24 +0530

    added register activity'

--- app/src/main/java/com/example/climateaction/MainActivity.java
+++ app/src/main/java/com/example/climateaction/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.climateaction;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fieldInitializer();
+    }
+
+    private void fieldInitializer() {
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        currentUser = firebaseAuth.getCurrentUser();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(currentUser==null){
+            sendToRegisterActivity();
+        }
+    }
+
+    private void sendToRegisterActivity() {
+
+        Intent RegisterIntent = new Intent(getApplicationContext(),RgisterActivity.class);
+        startActivity(RegisterIntent);
+
+
     }
 }
--- app/src/main/java/com/example/climateaction/SettingActivity.java
+++ app/src/main/java/com/example/climateaction/SettingActivity.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class SettingActivity extends AppCompatActivity {
 
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser currentUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        fieldInitializer();
+
+    }
+
+    private void fieldInitializer() {
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        currentUser = firebaseAuth.getCurrentUser();
+
     }
 }
