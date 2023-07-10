e956416101a94bd46bc31e5c3308f62e516f6962
Shivansh Kumar <codes.stalker@gmail.com>
2023-07-10 10:19:42 +0530

    DB Integrated

--- app/src/main/java/com/example/climateaction/addItem.java
+++ app/src/main/java/com/example/climateaction/addItem.java
@@ -1,5 +1,6 @@
 package com.example.climateaction;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,6 +8,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class addItem extends AppCompatActivity {
     private EditText item_name;
@@ -14,26 +25,75 @@ public class addItem extends AppCompatActivity {
     private Button add_btn;
     private String product_name,product_discription;
 
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser currentUser;
+
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
-        founder();
+        feildInitializer();
+
         add_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                product_name=item_name.getText().toString();
-                product_discription=item_discription.getText().toString();
-                Intent i =new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(i);
+               AddingItem();
             }
         });
 
     }
-    private void founder(){
+
+    private void AddingItem() {
+
+        product_name=item_name.getText().toString();
+        product_discription=item_discription.getText().toString();
+        String UID = currentUser.getUid();
+
+        HashMap<String, String> itemMap = new HashMap<>();
+        itemMap.put("uid", UID);
+        itemMap.put("product_name", product_name);
+        itemMap.put("product_description", product_discription);
+
+
+        databaseReference.child("users").child("items").child(UID).push().setValue(itemMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    sendToMainActivity();
+                    Toast.makeText(getApplicationContext(), "Profile Updated", Toast.LENGTH_SHORT).show();
+                }else{
+                    String error = task.getException().toString();
+                    Toast.makeText(getApplicationContext(), "Error :" + error, Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+
+
+    }
+
+    private void sendToMainActivity() {
+
+        Intent mainIntent =new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(mainIntent);
+
+    }
+
+    private void feildInitializer() {
         item_name=findViewById(R.id.item_name);
         item_discription=findViewById(R.id.item_discription);
         add_btn=findViewById(R.id.add_btn);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        currentUser = firebaseAuth.getCurrentUser();
     }
+
 
 }
--- app/src/main/java/com/example/climateaction/add_campaignActivity.java
+++ app/src/main/java/com/example/climateaction/add_campaignActivity.java
@@ -1,5 +1,6 @@
 package com.example.climateaction;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,32 +8,97 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class add_campaignActivity extends AppCompatActivity {
     private EditText title_btn, date_btn, camp_description;
     private Button add_camp_btn;
     private String title,date,description;
 
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser currentUser;
+
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_campaign);
+
+
         findId();
         add_camp_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                title=title_btn.getText().toString();
-                date=date_btn.getText().toString();
-                description=camp_description.getText().toString();
-                Intent i=new Intent(getApplicationContext(), campaign_acticity.class);
-                startActivity(i);
+               addCampaign();
             }
         });
     }
+
+    private void addCampaign() {
+
+        title=title_btn.getText().toString();
+        date=date_btn.getText().toString();
+        description=camp_description.getText().toString();
+        String UID = currentUser.getUid();
+
+
+        HashMap<String, String> campaignMap = new HashMap<>();
+        campaignMap.put("uid", UID);
+        campaignMap.put("title", title);
+        campaignMap.put("date", date);
+        campaignMap.put("description", description);
+
+        databaseReference.child("users").child("campaign").child(UID).push().setValue(campaignMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    sendToMainActivity();
+                    Toast.makeText(getApplicationContext(), "Campaign Added", Toast.LENGTH_SHORT).show();
+                }else{
+                    String error = task.getException().toString();
+                    Toast.makeText(getApplicationContext(), "Error :" + error, Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+
+        Intent i=new Intent(getApplicationContext(), campaign_acticity.class);
+        startActivity(i);
+
+
+    }
+
+    private void sendToMainActivity() {
+
+
+        Intent mainIntent =new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(mainIntent);
+
+    }
+
     private void findId(){
         title_btn=findViewById(R.id.title_btn);
         date_btn=findViewById(R.id.date_btn);
         camp_description=findViewById(R.id.camp_description);
         add_camp_btn=findViewById(R.id.add_camp_btn);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        currentUser = firebaseAuth.getCurrentUser();
     }
 }
